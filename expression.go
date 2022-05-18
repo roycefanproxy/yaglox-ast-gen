8c9b5f1823b1d9e7a00dbdd07041e6b28ca9d718
royce.fan <royce.fan@bytedance.com>
2022-05-19 03:09:03 +0800

    fix: file path

--- expression.go
+++ expression.go
@@ -1,5 +1,8 @@
 package main
 
+const TemplateSource = `
+package main
+
 import "github.com/roycefanproxy/yaglox"
 
 type {{.BaseName}} struct {
@@ -11,3 +14,4 @@ type {{$def.Name}} struct {
     {{range $j, $member := $def.Members}}{{$member}}{{"\n\t"}}{{end}}
 }
 {{end}}
+`
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"io/fs"
 	"os"
 	"path"
-	"path/filepath"
 	"text/template"
 )
 
@@ -49,25 +48,16 @@ func main() {
 }
 
 func defineAST(outputDir, interfaceName string, defs []*Definition) {
-	exePwd, err := os.Executable()
-	tmplDir := filepath.Dir(exePwd)
-	fmt.Println("template path: ", tmplDir)
-	tmplFilePath := path.Join(tmplDir, "expression.go.tmpl")
 	filePath := path.Join(outputDir, "expression.go")
 
-	tmplStr, err := os.ReadFile(tmplFilePath)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	tmpl := template.Must(template.New("expressions").Parse(string(tmplStr)))
+	tmpl := template.Must(template.New("expressions").Parse(TemplateSource))
 
 	buf := &bytes.Buffer{}
 	data := &TemplateData{
 		BaseName:    interfaceName,
 		Definitions: defs,
 	}
-	err = tmpl.Execute(buf, data)
+	err := tmpl.Execute(buf, data)
 	if err != nil {
 		panic(fmt.Sprintln("template hydration failed: ", err.Error()))
 	}
