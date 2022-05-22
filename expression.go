718954472d320c825ace231dfef885beb5f9e7fd
royce.fan <royce.fan@bytedance.com>
2022-05-22 20:14:23 +0800

    feat: add ast printer

--- expression.go
+++ expression.go
@@ -3,13 +3,24 @@ package main
 const TemplateSource = `
 package main
 
-type {{.BaseName}} interface {
+type Visitor[R any] interface {
+{{range $i, $def := .Definitions}}
+    Visit{{$def.Name}}{{$.BaseName}}(expr *{{$def.Name}}) R
+{{end}}
+}
 
+type {{.BaseName}} interface {
+    Accept(visitor Visitor[string]) string
 }
 
 {{range $i, $def := .Definitions}}
 type {{$def.Name}} struct {
     {{range $j, $member := $def.Members}}{{$member}}{{"\n\t"}}{{end}}
 }
+
+func (e *{{$def.Name}}) Accept(visitor Visitor[string]) string {
+    return visitor.Visit{{$def.Name}}{{$.BaseName}}(e)
+}
 {{end}}
+
 `
