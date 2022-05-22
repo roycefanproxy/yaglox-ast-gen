d06ba8a3ab6126bcbc54a128a8b00fe296dbd4ea
royce.fan <royce.fan@bytedance.com>
2022-05-23 03:27:53 +0800

    fix: duplicative visitor naming

--- expression_tmpl.go
+++ expression_tmpl.go
@@ -3,22 +3,23 @@ package main
 const TemplateSource = `
 {{$defs := .Definitions}}
 {{$visitors := .VisitorTypes}}
+{{$baseName := .BaseName}}
 package main
 
-type Visitor[R any] interface {
-    {{range $i, $def := $defs}}Visit{{$def.Name}}{{$.BaseName}}(expr *{{$def.Name}}) R{{if ne (len $defs) (add $i 1)}}{{"\n\t"}}{{end}}{{end}}
+type {{$baseName}}Visitor[R any] interface {
+    {{range $i, $def := $defs}}Visit{{$def.Name}}(expr *{{$def.Name}}) R{{if ne (len $defs) (add $i 1)}}{{"\n\t"}}{{end}}{{end}}
 }
 
-type {{.BaseName}} interface {
-    {{range $j, $visitor := $visitors}}Accept{{$visitor.Name}}(visitor Visitor[{{$visitor.Type}}]) {{$visitor.Type}}{{if ne (len $visitors) (add $j 1)}}{{"\n\t"}}{{end}}{{end}}
+type {{$baseName}} interface {
+    {{range $j, $visitor := $visitors}}Accept{{$visitor.Name}}(visitor {{$baseName}}Visitor[{{$visitor.Type}}]) {{$visitor.Type}}{{if ne (len $visitors) (add $j 1)}}{{"\n\t"}}{{end}}{{end}}
 }
 {{range $def := $defs}}
 type {{$def.Name}} struct {
     {{range $k, $member := $def.Members}}{{$member}}{{if ne (len $def.Members) (add $k 1)}}{{"\n\t"}}{{end}}{{end}}
 }
 {{range $visitor := $visitors}}
-func (e *{{$def.Name}}) Accept{{$visitor.Name}}(visitor Visitor[{{$visitor.Type}}]) {{$visitor.Type}} {
-    return visitor.Visit{{$def.Name}}{{$.BaseName}}(e)
+func (e *{{$def.Name}}) Accept{{$visitor.Name}}(visitor {{$baseName}}Visitor[{{$visitor.Type}}]) {{$visitor.Type}} {
+    return visitor.Visit{{$def.Name}}(e)
 }
 {{end}}
 {{end}}
